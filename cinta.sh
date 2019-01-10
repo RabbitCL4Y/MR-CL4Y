2b7314b5d1de3033b5922d7f044ffb2dad1af5d6
RabbitCL4Y <44805762+RabbitCL4Y@users.noreply.github.com>
2019-01-10 09:26:00 +0800

    Create cinta.sh

---
+++ cinta.sh
@@ -0,0 +1,214 @@
+#!/bin/bash
+#Kalo mau recode izin dulu sama author
+#Bisa hubungi lewat Email: RabbitCL4Y@gmail.com
+#Author: MR.CL4Y
+# Bersihkan Layar
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+cowsay -f eyes "USER" |lolcat
+echo ""
+echo $green"MASUKAN USERNAME DENGAN BENAR"
+echo $red"KALAU TIDAK TAHU USERNAMENYA HUB GW"
+echo $yellow"Whatsapp : 085752481601"
+echo ""
+echo $cyan"##############<USERNAME>###################"
+read -p"Masukan Username: " us
+if [ $us = "RabbitCL4Y" ];then
+sleep 1
+clear
+else
+clear
+cowsay -f eyes "Salah" | lolcat
+cowsay -f eyes "Failed" | lolcat
+sleep 1
+clear
+sh pw+us.sh
+fi
+
+cowsay -f eyes "PASSWORD" | lolcat
+echo ""
+echo $cyan"MASUKAN PASSWORD DGN BENAR"
+echo $purple"KALAU GK TAHU PASSWORDNYA HUB GW"
+echo $yellow"Whastapp : 085752481601"
+echo ""
+echo $green"#############PASSWORDNYA####################"
+read -p"Masukan Password: " pw
+if [ $pw = "Rafli" ];then
+sleep 1
+clear
+else
+clear
+toilet -f big -F gay "Salah"
+toilet -f big -F gay "Failed"
+sleep 1
+clear
+sh pw+us.sh
+fi
+
+toilet -f big -F gay "T00LS"
+toilet -f big -F gay "MR.CL4Y"
+toilet -f big -F gay "CARI CEWE"
+echo
+###################################################
+# CTRL C-
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, 
+Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My 
+Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Rabbit Cyber Team Was 
+Here ... "
+sleep 5
+read enter
+exit
+}
+=================================================
+echo $yellow
+figlet -f smshadow "TOOLS MR.CL4Y"
+echo $indigo
+date
+echo $purple"___________________________________________________"
+echo $cyan" ["$red"+"$cyan"]Author by	: MR.CL4Y "
+echo $cyan" ["$red"+"$cyan"]Team        : Rabbit Cyber Team		"
+echo $cyan" ["$red"+"$cyan"]Github	: https://github.com/RabbitCL4Y"
+echo $cyan" ["$red"+"$cyan"]Whatsapp : 085752481601"
+echo $cyan" ["$red"+"$cyan"]Instagram : muhammadrafli_337"
+echo $cyan" ["$red"+"$cyan"]Facebook : Rafli Mhmmd"
+echo $purple"___________________________________________________"
+echo ""
+toilet -f big -F gay "Selamat Datang"
+sleep 1
+echo $green"tool buat jones kampank"
+sleep 2
+echo $purple"___________________________________________________"
+echo
+echo $cyan" 1) FEMALE"
+sleep 3
+echo $purple"___________________________________________________"
+echo
+echo $blue" 2) MALE"
+sleep 4
+echo $purple"___________________________________________________"
+echo  $white""
+read -p "[root@MRCL4Y]> " bos;
+
+if [ $bos= 1 ] || [ $bos = 1 ]
+then
+clear
+figlet "WHATSAPP" | lolcat
+figlet "CEWE" | lolcat
+echo $yellow"NAMA = Annisa insani"
+echo $cyan"negara    = Indonesia"
+echo $blue"whatsapp= 08980101759"
+echo $purple"___________________________________________________"
+echo
+echo $blue"NAMA = Alfina"
+echo $green"negara = indonesia"
+echo $purple"whatsapp = 081395105653"
+echo $purple"___________________________________________________"
+echo
+echo $yellow"NAMA = Bellnabell"
+echo $cyan"negara = indonesia"
+echo $green"whatsapp = 087856398907"
+echo $purple"___________________________________________________"
+echo
+echo $ungu"NAMA = RIFAA"
+echo $purple" negara = indonesia"
+echo $cyan"whatsapp = 089639088323"
+echo $purple"___________________________________________________"
+echo
+echo $green"NAMA = Haeni Amalya"
+echo $cyan"Negara = indonesia"
+echo $blue"whatsapp = 085888631622"
+echo $purple"___________________________________________________"
+echo
+echo $blue"NAMA = Khadijah Azll"
+echo $white"negara = indonesia"
+echo $yellow"whatsapp = 089694889183"
+echo $purple"___________________________________________________"
+echo
+echo $white"NAMA = dewi"
+echo $yellow"negara = indonesia"
+echo $purple"whatsapp = 089688828424"
+echo $purple"___________________________________________________"
+echo
+echo $green"NAMA = hasna azizah"
+echo $ungu"negara = indonesia"
+echo $yellow"whatsapp = 085813043584"
+echo $purple"___________________________________________________"
+echo $green" 1)lanjut tools"
+read -p"Your Answer: " gg;
+fi
+
+if [ $gg = 1 ] || [ $gg = 1 ]
+then
+clear
+sh cinta.sh
+fi
+
+if [ $bos = 2 ] || [ $bos = 2 ]
+then
+clear
+figlet "WHATSAPP" | lolcat
+figlet "COWO" | lolcat
+echo $blue"NAMA = Apridal Balaw"
+echo $cyan"negara = indonesia"
+echo $green"whatsapp = 083807359899"
+echo $purple"___________________________________________________"
+echo
+echo $cyan"NAMA = ARIF"
+echo $blue"negara = indonesia"
+echo $white"whatsapp = 087888834330"
+echo $purple"___________________________________________________"
+echo
+echo $green"NAMA = Rafell"
+echo $blue"negara = indonesia"
+echo $cyan"whatsapp = 081316826548"
+echo $purple"___________________________________________________"
+echo
+echo $green"NAMA = Indra Alfarezi"
+echo $red"negara = indonesia"
+echo $yellow"whatsapp = 081254159753"
+echo $purple"___________________________________________________"
+echo
+echo $red"NAMA = Agus Irmawan"
+echo $green" negara = indonesia"
+echo $cyan"whatsapp = 085838737713"
+echo $purple"___________________________________________________"
+echo
+echo $white"NAMA = Iksan"
+echo $blue"negara = indonesia"
+echo $yellow"whatsapp = 081290437229" 
+echo $purple"___________________________________________________"
+echo
+echo $yellow"NAMA = YUNUS"
+echo $white"negara = indonesia"
+echo $green"whatsapp = 082141340626"
+echo $purple"___________________________________________________"
+echo
+echo $purple"NAMA = WAHYUDI"
+echo $white"negara = indonesia"
+echo $green"whatsapp = 083180836222"
+echo $purple"___________________________________________________"
+echo $green" 1)lanjut tools"
+read -p"Your Answer: " gg;
+fi
+
+if [ $gg = 1 ] || [ $gg = 1 ]
+then
+clear
+sh cinta.sh
+fi
