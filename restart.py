709d221f001ec427edc8cbe1efea14c584781a52
RabbitCL4Y <44805762+RabbitCL4Y@users.noreply.github.com>
2019-01-10 09:20:15 +0800

    Create restart.py

---
+++ restart.py
@@ -0,0 +1,14 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('WELCOME IN MY TOOLS..')
+mengetik('.')
+mengetik('.')
+mengetik('menghubungi MR.CL4Y..')
+mengetik ('Please Wait !!')
+mengetik ('WELCOME..!!')
